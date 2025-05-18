393f32f5e8c4bf480e8a5b01c8d59ba56366dcee
benya7 <bitup.games@gmail.com>
2025-05-18 19:04:41 +0200

    update: remove vite and simplify exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-export * from './constants';
-export * from './schema';
-export * from './types';
+export {
+  ID_PROPERTY,
+  RELEASE_NAME_PROPERTY,
+  RELEASE_CATEGORY_ID_PROPERTY,
+  RELEASE_CONTENT_CID_PROPERTY,
+  RELEASE_THUMBNAIL_CID_PROPERTY,
+  RELEASE_METADATA_PROPERTY,
+  FEATURED_RELEASE_ID_PROPERTY,
+  FEATURED_START_TIME_PROPERTY,
+  FEATURED_END_TIME_PROPERTY,
+  FEATURED_PROMOTED_PROPERTY,
+  CONTENT_CATEGORY_DISPLAY_NAME_PROPERTY,
+  CONTENT_CATEGORY_DESCRIPTION_PROPERTY,
+  CONTENT_CATEGORY_FEATURED_PROPERTY,
+  CONTENT_CATEGORY_METADATA_SCHEMA_PROPERTY,
+  SUBSCRIPTION_SITE_ID_PROPERTY,
+  SUBSCRIPTION_NAME_PROPERTY,
+  BLOCKED_CONTENT_CID_PROPERTY,
+} from './constants';
+export {
+  Release,
+  IndexableRelease,
+  FeaturedRelease,
+  ContentCategory,
+  Subscription,
+  BlockedContent,
+  Site,
+} from './schema';
+export type {
+  AnyObject,
+  IdData,
+  ReleaseData,
+  FeaturedReleaseData,
+  ContentCategoryData,
+  ContentCategoryMetadata,
+  SubcriptionData,
+  BlockedContentData,
+  AddReleaseResponse,
+  ILensService,
+} from './types';
+
+export { ElectronLensService, BrowserLensService } from './service';
