aa6625ff95eb835912a27ff30e2f28b92bb4b9f3
Benjamin Arntzen <zorlin@gmail.com>
2025-08-03 14:18:50 +0100

    More cleanup for RBAC

--- src/programs/acl/rbac/index.ts
+++ src/programs/acl/rbac/index.ts
@@ -1,2 +1,4 @@
 export * from './program';
 export * from './schemas';
+// Compatibility export for migration only
+export { RoleBasedccessController as RoleBasedccessControllerCompat } from './program-compat';
