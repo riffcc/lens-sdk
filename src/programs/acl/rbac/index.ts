bd8ccc39973ab04ed236ab0c1855f4d56dcafd72
Benjamin Arntzen <zorlin@gmail.com>
2025-08-03 15:08:54 +0100

    Remove compat layer - hard migration

--- src/programs/acl/rbac/index.ts
+++ src/programs/acl/rbac/index.ts
@@ -1,4 +1,4 @@
 export * from './program';
 export * from './schemas';
-// Compatibility export for migration only
-export { RoleBasedccessController as RoleBasedccessControllerCompat } from './program-compat';
+// Compatibility export for migration only - DO NOT import unless needed for migration
+// export { RoleBasedccessController as RoleBasedccessControllerCompat } from './program-compat';
